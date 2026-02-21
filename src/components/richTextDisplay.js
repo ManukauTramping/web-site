3a88652390b8abdac56d247673626ebbf05276c8
Colin Henwood <aretenz@users.noreply.github.com>
2026-02-21 21:00:48 +1300

    Fix rendering of link to pdf asset

--- src/components/richTextDisplay.js
+++ src/components/richTextDisplay.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 import { renderRichText } from "gatsby-source-contentful/rich-text"
-import { INLINES } from "@contentful/rich-text-types"
+import { BLOCKS, INLINES } from "@contentful/rich-text-types"
 import MediaLink from "./mediaLink"
 import AssetLink from "./assetLink"
 
@@ -11,9 +11,7 @@ const RichTextDisplay = ({ richText }) => {
     renderNode: {
       [INLINES.ASSET_HYPERLINK]: node => {
         const target = node.data.target
-        return target.fields
-          ? <MediaLink file={target.fields.file['en-NZ']} content={node.content[0]} />
-          : <AssetLink id={target.sys.id} content={node.content[0]} />
+        return <AssetLink id={target.sys.id} content={node.content[0].value} />
       },
 
       [INLINES.HYPERLINK]: node => {
@@ -29,17 +27,25 @@ const RichTextDisplay = ({ richText }) => {
         )
       },
 
-      [INLINES.EMBEDDED_ENTRY]: node => {
+      [BLOCKS.EMBEDDED_ASSET]: node => {
         const target = node.data.target
-        //const contentTypeId = target.sys.contentType.sys.id
-
-        // if (contentTypeId === 'information-page')
-        //   return RenderPageEntry(target.fields)
-        // else if (contentTypeId === 'contact')
-        //   return RenderContactEntry(target.fields)
-        // else
-          return null //documentToReactComponents(node.content)
+        return <AssetLink id={target.sys.id} content={''} />
       },
+
+      [INLINES.EMBEDDED_RESOURCE]: node => {return <></>},
+      [BLOCKS.EMBEDDED_RESOURCE]: node => {return <></>},
+      [BLOCKS.EMBEDDED_ENTRY]: node => {return <></>},
+      [INLINES.EMBEDDED_ENTRY]: node => {return <></>},
+      //   const target = node.data.target
+      //   //const contentTypeId = target.sys.contentType.sys.id
+      //
+      //   // if (contentTypeId === 'information-page')
+      //   //   return RenderPageEntry(target.fields)
+      //   // else if (contentTypeId === 'contact')
+      //   //   return RenderContactEntry(target.fields)
+      //   // else
+      //     return <></> //documentToReactComponents(node.content)
+      // },
     },
   }
 
