481cdd99359e042d05c79d15f9c17b7492fc669c
Colin Henwood <aretenz@users.noreply.github.com>
2026-02-21 21:20:52 +1300

    Open pdf on new tab (#143)

--- src/components/mediaLink.js
+++ src/components/mediaLink.js
@@ -3,7 +3,7 @@ import React from "react"
 const MediaLink = ({ file, content }) => 
     file.contentType.startsWith('image')
       ? <img src={file.url} alt={content} />
-      : <a href={file.url} type={file.contentType} className={'link-color'}>
+      : <a href={file.url} type={file.contentType} className={'link-color'} target="_blank" rel="noopener noreferrer">
             {content}
         </a>
 
