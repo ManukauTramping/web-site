7d8ee9e1cd65ad737ee6100449c85bdf2bf98f4e
Colin Henwood <aretenz@users.noreply.github.com>
2026-02-23 14:35:13 +1300

    Upgrade to Gatsby 5 (#150)

--- src/components/photoLinks.js
+++ src/components/photoLinks.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import { Link } from 'gatsby'
-import Img from "gatsby-image";
+import { GatsbyImage } from "gatsby-plugin-image";
 
 const PhotoLinks = ({ links }) => {
 
 	return (
-    <div className={'columns is-multiline'}>
-      {links.map(({ caption, page, photo }) =>
-        <div key={caption} className={'column is-half-tablet is-one-quarter-desktop'}>
-          <Link key={page.slug} to={`/${page.slug}`}>
-            <div className={'is-relative'}>
-              <Img key={photo.id} fluid={photo.fluid} title={caption} fadeIn={false} />
-              <div className={'is-size-3 has-text-centered has-text-white is-overlay photolink-text'}>
-                {caption}
+      <div className={'columns is-multiline'}>
+        {links.map(({ caption, page, photo }) =>
+          <div key={caption} className={'column is-half-tablet is-one-quarter-desktop'}>
+            <Link key={page.slug} to={`/${page.slug}`}>
+              <div className={'is-relative'}>
+                <GatsbyImage
+                  image={photo.gatsbyImageData}
+                  key={photo.id}
+                  title={caption}
+                  fadeIn={false}
+                  alt={caption}
+                />
+                <div className={'is-size-3 has-text-centered has-text-white is-overlay photolink-text'}>
+                  {caption}
+                </div>
               </div>
-            </div>
-          </Link>
-        </div>
-      )}
-    </div>
-  )
+            </Link>
+          </div>
+        )}
+      </div>
+    );
 }
 
 export default PhotoLinks;
