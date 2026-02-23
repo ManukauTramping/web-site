6827ef278da69571d4cbb4f9b1e82e0976d99355
Colin Henwood <aretenz@users.noreply.github.com>
2026-02-24 02:40:55 +1300

    add gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,6 +59,7 @@ module.exports = {
 		},
 		`gatsby-plugin-layout`,
 		`gatsby-plugin-sitemap`,
+		'gatsby-plugin-netlify',
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.app/offline
 		// 'gatsby-plugin-offline',
