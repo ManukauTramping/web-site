d14cff8bd745430951bb127390cfddf6c21e30e7
Colin Henwood <aretenz@users.noreply.github.com>
2026-02-23 14:44:35 +1300

    Use Netlify Adapter

--- gatsby-config.js
+++ gatsby-config.js
@@ -59,7 +59,6 @@ module.exports = {
 		},
 		`gatsby-plugin-layout`,
 		`gatsby-plugin-sitemap`,
-		`gatsby-plugin-netlify`
 		// this (optional) plugin enables Progressive Web App + Offline functionality
 		// To learn more, visit: https://gatsby.app/offline
 		// 'gatsby-plugin-offline',
